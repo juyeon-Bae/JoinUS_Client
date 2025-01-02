c0089cb591d1b967ef58898d7743cd7733b48ff6
3x-haust <lyusungyun@icloud.com>
2025-01-02 14:16:31 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyle';
+import './locales/i18n';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GlobalStyles />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
